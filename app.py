ad9fffc16b22ebef71ba0540c25005f597fe29c4
Raffaele Rizzardi <rizzardi.raffaele@iisgalvanimi.edu.it>
2024-12-09 12:04:01 +0000

    svolta prima parte, login e registrazione

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = 'key_sessione_user'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+    
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html', username=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            return render_template('register.html', error="Questo username è già in uso.")
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', error=None)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            flash("login effettuato!", "success")
+            return redirect(url_for('home'))
+        flash("credenziali non valide!", "error")
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
