c4bfaec7fe1a2c0d8dcaf677001b841ead8221b0
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2025-01-06 20:59:22 +0000

    finito we hope

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, send_from_directory
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from models import db, User
 from flask_bcrypt import Bcrypt
+from flask_pymongo import PyMongo
+import os
+from werkzeug.security import check_password_hash, generate_password_hash
+from models import User
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.secret_key = 'key_sessione_user'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-
-db.init_app(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/musicDB"
+app.secret_key = os.urandom(24) 
+mongo = PyMongo(app)
+
+login_manager.init_app(app)
+
+
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
+def load_user(username):
+    return User.get(username) 
 
 @app.route('/home')
 @login_required
 def home():
-    return render_template('home.html', username=current_user.username)
+    songs = mongo.db.songs.find()
+    return render_template('home.html', username=current_user.username, songs=songs)
 
 @app.route('/logout')
 @login_required
@@ -34,14 +40,18 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if User.query.filter_by(username=username).first():
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        existing_user = mongo.db.users.find_one({"username": username})
+        if existing_user:
             flash("Questo username è già in uso.", "error")
             return render_template('register.html')
 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
+        mongo.db.users.insert_one({
+            "username": username,
+            "password": hashed_password
+        })
         flash("Registrazione avvenuta con successo!", "success")
         return redirect(url_for('login'))
     return render_template('register.html')
@@ -51,8 +61,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
+
+        user = User.get(username)
+        if user and bcrypt.check_password_hash(user.password, password): 
             login_user(user)
             flash("Login effettuato con successo!", "success")
             return redirect(url_for('home'))
@@ -60,8 +71,32 @@ def login():
         return redirect(url_for('login'))
     return render_template('login.html')
 
-with app.app_context():
-    db.create_all()
+@app.route('/add_song', methods=['GET', 'POST'])
+@login_required
+def add_song():
+    if request.method == 'POST':
+        title = request.form['title']
+        artist = request.form['artist']
+        audio = request.files['audio']
+
+        if audio:
+            audio_path = os.path.join('uploads', audio.filename)
+            audio.save(audio_path)  
+            mongo.db.songs.insert_one({
+                "title": title,
+                "artist": artist,
+                "filename": audio.filename,
+                "user": current_user.username
+            })
+            flash("Canzone aggiunta con successo!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Errore: nessun file audio selezionato.", "error")
+    return render_template('add_song.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,9 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-db = SQLAlchemy()
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    def get_id(self):
+        return self.username
+
+    @classmethod
+    def get(cls, username):
+        from app import mongo  
+        user_data = mongo.db.users.find_one({"username": username})
+        if user_data:
+            return cls(user_data["username"], user_data["password"])
+        return None
+
+
+
+
